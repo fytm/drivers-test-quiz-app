5c404ed726f93e377b7f57aa0f52752a7b0ea76a
felix.tamakloe <felix.tamakloe@turntabl.io>
2023-11-01 09:19:08 +0000

    MVP

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,57 +1,104 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ConstrainedExecution;
 
 internal class Program
 {
-    record class Question
-    {
-        public String text{ get; }
-        private List<string> options;
-        private string answer;
 
+    private static List<QuestionData> AllQuestions = new List<QuestionData>();
+    private static string csvFilePath = "C:\\Users\\felix.tamakloe\\Desktop\\Spikes\\C#\\ConsoleApps\\question_data.csv";
 
-        public Question(string text, List<string> options, int ans_pos)
-        {
-            this.text = text;
-            this.options = options;
-            this.answer = options[ans_pos];
-        }
-    }
-
-
-    //public static void play()
-    //{
-    //    console.writeline("hello")
-
-    //}
-    private static void AskQuestions(List<Question> myQuestions)
+    private static void AskQuestions()
     {
         int randomNumber = Random.Shared.Next();
-        int numQuestions = myQuestions.Count;
+        int numQuestions = AllQuestions.Count;
         Boolean isPractising = true;
         while (isPractising)
         {
-            Question question = myQuestions[randomNumber % numQuestions];
-            question.ToString();
-            Console.WriteLine(question.ToString());
+            if (numQuestions == 0 )
+            {
+                Console.WriteLine("Out of questions. Exiting.....");
+                break;
+            }
+            int MyQuestionIndex = randomNumber % numQuestions;
+            QuestionData question = AllQuestions[MyQuestionIndex];
+            Console.WriteLine(question.Question.ToString());
+            question.showOptions();
+            AllQuestions.Remove(question);
+            //myQuestions.RemoveAt(MyQuestionIndex);
             Console.WriteLine("Press q to exit or anything else to continue");
             if (Console.ReadLine() == "q")
             {
                 isPractising = false;
             }
+            numQuestions = numQuestions-1;
         }
     }
 
+    private static void LoadQuestions()
+    {
+        // Create a configuration for CsvHelper..
+        var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+            Delimiter = ",",
+        };
+        // Read the CSV file.
+        using (var reader = new StreamReader(csvFilePath))
+        using (var csv = new CsvReader(reader, csvConfig))
+        {
+            IEnumerable<QuestionData> records = csv.GetRecords<QuestionData>();
+            Console.WriteLine("Reading questions into app");
+            foreach (var record in records)
+            {
+                AllQuestions.Add(record);
+            }
+        }
+    }
+
+
+
     private static void Main(string[] args)
     {
-        List<Question> myQuestions = new List<Question>();
-        List<string> options = new List<string>();
-        options.Add("Stop");
-        options.Add("Go");
-        myQuestions.Add(new Question("What does a red light mean?", options, 0));
         Console.WriteLine("This application helps you prepare for the drivers test by asking you a few practice questions!");
-        AskQuestions(myQuestions);
+        LoadQuestions();
+        AskQuestions();
     }
-   
+
+    public class QuestionData
+    {
+        public String Question { get; set; }
+        public HashSet<string> Options { get; set; }
+        private string Answer { get; set; }
+
+
+        public QuestionData(string Question, string Options, string Answer)
+        {
+            this.Question = Question;
+            this.Options = new HashSet<string>();
+            foreach (var option in Options.Split(",")) 
+                {
+                this.Options.Add(option);
+                    }
+            this.Options.Add(Answer);
+            this.Answer = Answer;
+        }
+
+        public string getAnswer()
+        {
+            return this.Answer;
+        }
+        public void showOptions()
+        {
+            foreach (var option in this.Options){
+                Console.Write($"{option.ToString()} |");
+            }
+            Console.WriteLine() ;
+        }
+    }
+
 }
 
 
