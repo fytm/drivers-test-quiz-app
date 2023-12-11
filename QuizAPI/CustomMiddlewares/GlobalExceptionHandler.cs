dc35e4e3cd7b515520301df037c9ad0cd939b050
Felix <ftamakloe62@gmail.com>
2023-12-11 22:25:23 +0000

    Added basic exception handling middleware (#3)
    * Added basic exception handling middleware
    
    * Added basic exception handling middleware
    
    * Improved exception handling
    
    ---------
    
    Co-authored-by: felix.tamakloe <felix.tamakloe@turntabl.io>

---
+++ QuizAPI/CustomMiddlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using QuizAPI.Exceptions;
+using System.Net;
+
+namespace QuizAPI.CustomMiddlewares
+{
+    public class GlobalExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(RequestDelegate next ,ILogger<GlobalExceptionHandler> logger)
+        {
+            _next = next;
+            _logger = logger;   
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception and generate a response
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+
+            context.Response.ContentType = "application/json";
+            var response = context.Response;
+            switch (exception)
+            {
+                case ResourceNotFoundException ex:
+                    response.StatusCode = ex.StatusCode;
+                    await response.WriteAsJsonAsync(ex.Value);
+                    break;
+                default:
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    await response.WriteAsJsonAsync("Internal server error!");
+                    break;
+            }
+            var exceptionMessage = exception.Message;
+            _logger.LogError(
+                "Error Message: {exceptionMessage}, Time of occurrence {time}",
+                exception.Message, DateTime.UtcNow);
+        }
+    }
+}
+
---
+++ QuizAPI/Exceptions/ResourceNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace QuizAPI.Exceptions
+{
+    public class ResourceNotFoundException : HttpResponseException
+    {
+        public ResourceNotFoundException(string resourceId) : base(
+            (int)HttpStatusCode.NotFound,
+            $"Resource with id {resourceId} not found")
+        {
+        }
+    }
+}
